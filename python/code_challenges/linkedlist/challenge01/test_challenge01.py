088eecca1a8d12299975e8b25341107e3f48cb79
anasknbar <knbaranasgithub@gmail.com>
2024-05-15 21:13:18 +0000

    challenge-01

--- python/code_challenges/linkedlist/challenge01/test_challenge01.py
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -1,1 +1,16 @@
 # Write your test here
+import pytest
+from challenge01 import Node,Linkedlist
+
+def test_delete_method():
+  my_list = Linkedlist()
+  my_list.append(10)
+  my_list.append(20)
+  my_list.append(30)
+  my_list.append(40)
+  befor_deleting = len(my_list)
+  
+  my_list.delete(40)
+  after_deleting = len(my_list)
+  
+  assert after_deleting == befor_deleting - 1
---
+++ python/code_challenges/linkedlist/challenge01/whiteboard.md
@@ -0,0 +1,5 @@
+# note:
+### in the challenge01.py file I had to write other method along with the delete method so I that I can test the delete function. it was so hard for me to figure out a way to test the delete method without other method like the appending method and the length method
+
+
+![my_whitebord for the code challenge 01](whitebord.png)
